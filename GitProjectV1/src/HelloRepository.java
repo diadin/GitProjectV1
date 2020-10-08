f2797640cc2f5ca348752fffbc98e0f0a65a403a
diana.moldoveanu <diana.moldoveanu@BUK30LAP4002052.client.ro.r4.madm.net>
2020-10-08 12:36:50 +0300

    Change HelloRepository in master. Add new line too.

--- GitProjectV1/src/HelloRepository.java
+++ GitProjectV1/src/HelloRepository.java
@@ -3,7 +3,8 @@ public class HelloRepository {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello in the world of GIT. MARI Changes something in master :). We learn how to work with GIT ");
+		System.out.println("Hello in the world of GIT. MARI Changes something in master :). ");
+		System.out.println(" We learn how to work with GIT" );
 
 	}
 
