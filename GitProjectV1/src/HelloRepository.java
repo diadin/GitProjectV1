29d1711e1db8158fd7fe36d52bce2b57445c29ce
Diana <out-of-sorts@yahoo.com>
2020-10-08 10:52:25 +0300

    Change HelloRepository from other project

--- GitProjectV1/src/HelloRepository.java
+++ GitProjectV1/src/HelloRepository.java
@@ -3,7 +3,7 @@ public class HelloRepository {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello in the world of GIT.Change again");
+		System.out.println("Hello in the world of GIT. We learn how to work");
 
 	}
 
