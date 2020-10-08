65ddf3d17f236413e07da63feb101845479c51b8
diana.moldoveanu <diana.moldoveanu@BUK30LAP4002052.client.ro.r4.madm.net>
2020-10-08 15:02:20 +0300

    Diana merge with squash (In master merge from branchX).Merge commit.

---
+++ GitProjectV1/src/HelloAgain.java
@@ -0,0 +1,11 @@
+public class HelloAgain {
+
+	public static void main(String[] args) {
+			
+		System.out.println("Hello from other branch!");
+		System.out.println("MARI SCHIMBA CEVA");
+		System.out.println("DIANA solves the conflict in BranchX");
+
+	}
+
+}
